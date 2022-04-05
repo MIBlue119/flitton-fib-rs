00e3e67fc9eadc2a8c01f58f079da95ce483e4b8
MIBlue119 <miblue119@gmail.com>
2022-04-05 22:48:41 +0800

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;      // It enable the macros pyo3 crate has
 use pyo3::wrap_pyfunction; // It let us wrap Rust function into module
 
 //Initial declare the module fib_calcs
-mode fib_calcs;
+mod fib_calcs;
 // Prefix `__pyo3_get_function_` let us to retain the macros applied to the functions
 use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
 use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
