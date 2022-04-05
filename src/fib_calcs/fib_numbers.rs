4d3378f39d92d9ac52e968ea58b09da8c2bf66d0
MIBlue119 <miblue119@gmail.com>
2022-04-05 22:53:36 +0800

    Fix

--- src/fib_calcs/fib_numbers.rs
+++ src/fib_calcs/fib_numbers.rs
@@ -2,7 +2,7 @@ use std::vec::Vec;
 use pyo3::prelude::pyfunction;
 use super::fib_number::fibonacci_number;
 
-#[pyfunciton]
+#[pyfunction]
 pub fn fibonacci_numbers(numbers: Vec<i32>)-> Vec<u64>
 {
     let mut vec: Vec<u64> = Vec::new();
