dc1f523116d258ba50b4cc1c5795a0e611c7ad50
MIBlue119 <miblue119@gmail.com>
2022-04-05 22:47:03 +0800

    Feat: add the fibonacci rust code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 use pyo3::prelude::*;      // It enable the macros pyo3 crate has
 use pyo3::wrap_pyfunction; // It let us wrap Rust function into module
 
+//Initial declare the module fib_calcs
+mode fib_calcs;
+// Prefix `__pyo3_get_function_` let us to retain the macros applied to the functions
+use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
+use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
+
+pub mod fib_numbers;
 
 //Apply a Python function macro from pyO3 to the say_hello function
 #[pyfunction]
@@ -15,5 +22,7 @@ fn say_hello(){
 fn flitton_fib_rs(_py: Python, m: &PyModule)->PyResult<()>{
         //wrap_pyfunction takes a Python instance  and resturn a python function
         m.add_wrapped(wrap_pyfunction!(say_hello));
+        m.add_wrapped(wrap_pyfunction!(fibonacci_number));
+        m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
         Ok(())
 }
